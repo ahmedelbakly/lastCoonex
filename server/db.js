1bae99010aa4313644727202f380e4f0e4ab247e
ahmed elbakly <ahmedelbakly258@gmail.com>
2023-10-07 00:42:47 +0300

    lastCommit

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+const DB = process.env.MONGO_URI.replace("<password>", process.env.DB_PASSWORD);
 
-// const DB = process.env.MONGO_URI.replace("<password>", process.env.DB_PASSWORD);
 
-const DB = "mongodb://127.0.0.1:27017/coonex"
+//const DB = process.env.MONGO_URI;
 
 const connectDB = () => {
   mongoose.set("strictQuery", false);
